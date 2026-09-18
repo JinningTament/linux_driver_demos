e863c5f20f667efdbbd7312870fc5d20a8b3d89f
luojin <luojin@localhost.com>
2026-09-18 10:58:05 +0800

    Fixed the mistake about 04_cdev

--- 04_cdev/cdev.c
+++ 04_cdev/cdev.c
@@ -6,38 +6,44 @@
 #include <linux/cdev.h>
 
 static dev_t dev_num;
-struct cdev cdev_test;
-struct file_operations cdev_test_ops={
-    .owner=THIS_MODULE
+static struct cdev cdev_test;
+static struct file_operations cdev_test_ops = {
+    .owner = THIS_MODULE
 };
 
 static int moduledev_init(void)
 {
     int ret;
+    int major, minor;
 
-    
-        ret = alloc_chrdev_region(&dev_num, 0, 1, "alloc_name");
-        if (ret < 0) {
-            printk(KERN_ERR "alloc_chrdev_region failed, ret=%d\n", ret);
-            return ret;
-        }
-        major = MAJOR(dev_num);
-        minor = MINOR(dev_num);
-        printk(KERN_INFO "alloc success, major=%d, minor=%d\n", major, minor);
+    ret = alloc_chrdev_region(&dev_num, 0, 1, "alloc_name");
+    if (ret < 0) {
+        printk(KERN_ERR "alloc_chrdev_region failed, ret=%d\n", ret);
+        return ret;
+    }
 
-        //init cdev
-        cdev_test.owner=THIS_MODULE;
-        cdev_init(&cdev_test,&cdev_test_ops);
-        cdev_add(&cdev_test,dev_num,1);
+    major = MAJOR(dev_num);
+    minor = MINOR(dev_num);
+    printk(KERN_INFO "alloc success, major=%d, minor=%d\n", major, minor);
 
+    cdev_init(&cdev_test, &cdev_test_ops);
+    cdev_test.owner = THIS_MODULE;
+
+    ret = cdev_add(&cdev_test, dev_num, 1);
+    if (ret < 0) {
+        printk(KERN_ERR "cdev_add failed, ret=%d\n", ret);
+        unregister_chrdev_region(dev_num, 1);
+        return ret;
+    }
+
+    printk(KERN_INFO "cdev_add success\n");
     return 0;
 }
 
 static void moduledev_exit(void)
 {
-    //Delete the device number first, then uninstall the character device
+    cdev_del(&cdev_test);
     unregister_chrdev_region(dev_num, 1);
-    cdev_del(dev_num);
     printk(KERN_INFO "seeyouworld!\n");
 }
 
